f1d29e5b656a8e00cb2b4aff6b3aa59eee015a64
superpplee <1023512730@qq.com>
2019-07-05 14:13:55 +0800

    添加购物车表

---
+++ ShopPojo/src/main/java/com/qphone/pojo/Shopcar.java
@@ -0,0 +1,65 @@
+package com.qphone.pojo;
+
+import java.util.Date;
+
+public class Shopcar {
+    private Integer id;
+
+    private Integer userId;
+
+    private Integer productId;
+
+    private Integer num;
+
+    private String status;
+
+    private Date createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
